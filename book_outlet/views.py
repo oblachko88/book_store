fb6cf37b72e6234a69f44dccac1ae5f358a5ec1b
oblako88 <88oblachko@gmail.com>
2023-10-31 15:33:42 +0200

    urls added

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Book
+
 
 # Create your views here.
 
 def index(request):
-  return render(request, "book_outlet/index.html")
+  books = Book.objects.all()
+  return render(request, "book_outlet/index.html", {
+    "books": books
+  })
+
+def book_detail(request, id):
+  book = Book.objects.get(pk=id) # pk stands for primary key defined in our database. 
+  return render(request, "book_outlet/index.html", {
+    "title": book.title,
+    "author": book.author,
+    "rating": book.rating,
+    "is_bestseller": book.is_bestseller
+  })
+  
