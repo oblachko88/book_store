bbc237d684c48f5b8c0b520a6b9c170d78dc3441
oblako88 <88oblachko@gmail.com>
2023-11-01 18:15:23 +0200

    module added to admin.py

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -5,6 +5,8 @@ from .models import Book
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-  readonly_fields = ("slug",)
+  prepopulated_fields = {"slug": ("title",)}
+  list_filter = ("author", "rating",)
+  list_display = ("title", "author",)
 
 admin.site.register(Book, BookAdmin)
