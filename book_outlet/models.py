645d2ff7acd8df5007e89f0e684d718000ddf83f
oblako88 <88oblachko@gmail.com>
2023-11-01 11:54:59 +0200

    slug func added

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,10 +11,14 @@ class Book(models.Model):
     validators=[MinValueValidator(1), MaxValueValidator(5)])
   author = models.CharField(null=True, max_length=100)
   is_bestselling = models.BooleanField(default=False) #So by default the book is not the best selling one.
+  slug = models.SlugField(default="", null=False, )
 
   def get_absolute_url(self):
       return reverse("book-detail", args=[self.id])
   
+  def save(self, *args, **kwargs):
+    self.slug = slugify(self.title)
+    super().save(*args, **kwargs)
 
   def __str__(self):
     return f"{self.title} ({self.rating}, Author: {self.author}, {self.is_bestselling}"
